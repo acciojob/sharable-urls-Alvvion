d41c18525bb3ad511f11ab03002df80a4325a92f
Utkarsh Verma <alvvion@gmail.com>
2025-09-03 20:11:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // your code here
+const h3 = document.querySelector("#heading")
+const name = document.querySelector("#name")
+const year = document.querySelector("#year")
+const button = document.querySelector("#button")
+const handleSubmit = () => {
+	h3.innerHTML = `https://localhost:8080/?name=${name.value}&year=${year.value}`
+}
+button.addEventListener("click", handleSubmit)
